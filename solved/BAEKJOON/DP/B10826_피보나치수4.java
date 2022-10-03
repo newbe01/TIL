69cfd847577ddd5c1a0f857a8c177105aa960121
newbe01 <pkwaa3@gmail.com>
2022-10-03 21:09:33 +0900

    commit

--- solved/BAEKJOON/DP/B10826_피보나치수4.java
+++ solved/BAEKJOON/DP/B10826_피보나치수4.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  * n=17일때 까지 피보나치 수를 써보면 다음과 같다.
  * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
  * n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
- */
+ */ 
 public class B10826_피보나치수4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -22,12 +22,12 @@ public class B10826_피보나치수4 {
     }
 
     public static void fibo(int n){
-        BigInteger arr[] = new BigInteger[10001];
+        BigInteger arr[] = new BigInteger[10001]; //수가 너무 커져서 biginteger로 계산해야함
 
         arr[0] = new BigInteger("0");
         arr[1] = new BigInteger("1");
 
-        for(int i = 2; i < arr.length; i++){
+        for(int i = 2; i < arr.length; i++){   
             arr[i] = arr[i - 2].add(arr[i - 1]);
         }
 
