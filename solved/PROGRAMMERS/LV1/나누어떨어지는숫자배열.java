c0bcf3ad7f94b18375f19cdc6cb4972a805e195c
newbe01 <pkwaa3@gmail.com>
2022-10-06 22:52:23 +0900

    commit

---
+++ solved/PROGRAMMERS/LV1/나누어떨어지는숫자배열.java
@@ -0,0 +1,49 @@
+package solved.PROGRAMMERS.LV1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, 
+ * solution을 작성해주세요.
+ * divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+ * 제한사항
+ * arr은 자연수를 담은 배열입니다.
+ * 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+ * divisor는 자연수입니다.
+ * array는 길이 1 이상인 배열입니다.
+ */
+public class 나누어떨어지는숫자배열 {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+
+        System.out.println(solution(arr,5));
+    }
+    public static int[] solution(int[] arr, int divisor) {
+        
+        ArrayList list = new ArrayList<>();
+        
+        System.out.println("hi");
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % divisor == 0){ //나머지는 수들을 list 에 담음
+                list.add(arr[i]); 
+            }
+        }
+        
+        int answer[];
+        if(list.size() == 0){   //값이없으면 -1 리턴
+            answer = new int[1];
+            answer[0] = -1;
+
+        }else{  //list 길이만큼 배열크기 지정후 배열에 담음
+            answer = new int[list.size()];
+            for(int i = 0; i < list.size(); i++){
+                answer[i] = (Integer)list.get(i);
+            }
+        }
+
+        Arrays.sort(answer);    //배열정렬
+        
+        return answer;
+    }
+}
--- solved/PROGRAMMERS/PROGRAMMERS.md
+++ solved/PROGRAMMERS/PROGRAMMERS.md
@@ -12,3 +12,4 @@
     * [콜라츠추측](https://github.com/newbe01/TIL/blob/main/solved/PROGRAMMERS/LV1/콜라츠추측.java)
     * [두정수사이의합](https://github.com/newbe01/TIL/blob/main/solved/PROGRAMMERS/LV1/두정수사이의합.java)
     * [서울에서김서방찾기](https://github.com/newbe01/TIL/blob/main/solved/PROGRAMMERS/LV1/서울에서김서방찾기.java)
+    * [나누어떨어지는숫자배열](https://github.com/newbe01/TIL/blob/main/solved/PROGRAMMERS/LV1/나누어떨어지는숫자배열.java)
